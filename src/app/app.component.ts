72f3423cb3b9e9b98658175a16fa6f99b4301911
Magd13 <joelmagdielriveraarce@gmail.com>
2023-11-02 13:42:29 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Camion } from './models/camion';
+import { Camioneta } from './models/camioneta';
+import { Automovil } from './models/automovil';
+import { Barco } from './models/barco';
+import { Yate } from './models/yate';
+import { Avion } from './models/avion';
+import { Avioneta } from './models/avioneta';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'herencias';
+  title: string = 'Mi Aplicación';
+  constructor() {
+    const camion = new Camion('MarcaCamion', 'ModeloCamion', 2023, 'Rojo', 80, 5000, 'Materiales');
+    const camioneta = new Camioneta('MarcaCamioneta', 'ModeloCamioneta', 2023, 'Azul', 100, 5, '4x4');
+    const automovil = new Automovil('MarcaAuto', 'ModeloAuto', 2023, 'Negro', 160, 'Gasolina', 4);
+    const barco = new Barco('MarcaBarco', 'ModeloBarco', 2023, 'Blanco', 50, 'Motor de Propulsión', 10);
+    const yate = new Yate('MarcaYate', 'ModeloYate', 2023, 'Amarillo', 120, 'Motor de Lujo', 5);
+    const avion = new Avion('MarcaAvion', 'ModeloAvion', 2023, 'Blanco', 400, 'Alas de Gran Envergadura', 200);
+    const avioneta = new Avioneta('MarcaAvioneta', 'ModeloAvioneta', 2023, 'Rojo', 100, 4, 'Alas Pequeñas');
+    console.log('camion:');
+    console.log(camion);
+    console.log('camioneta:');
+    console.log(camioneta);
+    console.log('automovil:');
+    console.log(automovil);
+    console.log('barco:');
+    console.log(barco);
+    console.log('yate:');
+    console.log(yate);
+    console.log('avion:');
+    console.log(avion);
+    console.log('avioneta:');
+    console.log(avioneta);
+    camion.mostrarPropiedades();
+    camioneta.mostrarPropiedades();
+    automovil.mostrarPropiedades();
+    barco.mostrarPropiedades();
+    yate.mostrarPropiedades();
+    avion.mostrarPropiedades();
+    avioneta.mostrarPropiedades();
+  }
 }
